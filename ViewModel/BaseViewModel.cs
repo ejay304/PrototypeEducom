63f1543565d040d0f974b222d7df5c9aec2291b5
Alain Fresco <alain.fresco@gmail.com>
2014-06-12 16:53:39 +0200

    Ajout de la fonctionnlite d'ajout de requrtes

---
+++ ViewModel/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using PrototypeEDUCOM.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace PrototypeEDUCOM.ViewModel
+{
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        protected EducomDb db = new EducomDb();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string nomPropriete)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(nomPropriete));
+        }
+    }
+}
