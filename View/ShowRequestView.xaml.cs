b3abc9442e092e924e4a0f717355748b226322bd
Alain Fresco <alain.fresco@gmail.com>
2014-06-12 15:21:10 +0200

    Modification du fichier .gitignore

---
+++ View/ShowRequestView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PrototypeEDUCOM.View
+{
+    /// <summary>
+    /// Logique d'interaction pour ShowRequestView.xaml
+    /// </summary>
+    public partial class ShowRequestView : Window
+    {
+        public ShowRequestView()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- ViewModel/RequestsViewModel.cs
+++ ViewModel/RequestsViewModel.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using PrototypeEDUCOM.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace PrototypeEDUCOM.ViewModel
 {
@@ -12,9 +14,16 @@ namespace PrototypeEDUCOM.ViewModel
         private Model.EducomDb db = new Model.EducomDb();
 
         public ICollection<Model.request> requests { get; set; }
+        public ICommand cmdViewDetail { get; set; }
 
         public RequestsViewModel() {
             this.requests = db.requests.ToArray();
+            this.cmdViewDetail = new RelayCommand<request>(actViewDetail);
+        }
+
+        public void actViewDetail(request request) {
+            View.RequestsView requestsView = new View.RequestsView();
+            requestsView.Show();
         }
     }
 }
