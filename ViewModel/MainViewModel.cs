4dd1cf0ba6bb85fced421d0fbcb8e7ea889e1586
Romain Therisod <romain.therisod@heig-vd.ch>
2014-06-30 11:23:45 +0200

    tab manage in view

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace PrototypeEDUCOM.ViewModel
 {
+    class Tab
+    {
+        public String header { get; set; }
+
+        public UserControl content { get; set; }
+
+        public Tab(String header, UserControl content)
+        {
+            this.header = header;
+            this.content = content;
+        }
+    }
+
     class MainViewModel : BaseViewModel
     {
 
@@ -17,6 +32,8 @@ namespace PrototypeEDUCOM.ViewModel
 
         public String message { get; set; }
 
+        public ObservableCollection<Tab> tabs { get; set; }
+
         public ICommand btnLogin { get; set; }
 
         public Action CloseAction { get; set; }
@@ -24,10 +41,16 @@ namespace PrototypeEDUCOM.ViewModel
         public MainViewModel()
         {
             btnLogin = new RelayCommand<object>(actLogin);
+
+            tabs = new ObservableCollection<Tab>();
+            tabs.Add(new Tab("Request", new View.RequestsUCView()));
+            tabs.Add(new Tab("Page", new View.UserControl2()));
+
         }
 
         private void actLogin(object arg)
         {
+
             // login admin@admin.com pass admin
             // login test@testcom pass test
             if (login.Length != 0 && pass.Length != 0)
